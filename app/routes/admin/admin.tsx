b89213a513cafd11e0e372c83bbb81bcfe54ca69
Melody Universe <melody@computost.com>
2025-05-13 22:01:56 -0700

    guard admin page

--- app/routes/admin/admin.tsx
+++ app/routes/admin/admin.tsx
@@ -1,5 +1,20 @@
 import type { ReactNode } from "react";
 
+import { data } from "react-router";
+
+import { getSession } from "~/sessions.server";
+
+import type { Route } from "./+types/admin";
+
 export default function Admin(): ReactNode {
   return <p>Welcome, admin!</p>;
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request);
+  const user = session.get("user");
+  if (!user?.isAdmin) {
+    // eslint-disable-next-line @typescript-eslint/only-throw-error
+    throw data("User is not an admin", { status: 404 });
+  }
 }
